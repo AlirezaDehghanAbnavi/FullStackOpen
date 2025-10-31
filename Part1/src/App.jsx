7556ec91d02074ac2de6f1bf40ce7a256d35cd8f
Alireza DehghanAbnavi <s308048@studenti.polito.it>
2025-10-31 17:34:22 +0100

    Fixes on Content and Total using props

--- Part1/src/App.jsx
+++ Part1/src/App.jsx
@@ -24,7 +24,7 @@ const Content = (props) => {
 
 const Total = (props) => {
   return (
-    <p> Number of exercises {props.noExercises}</p>
+    <Part Number of exercises={props.parts[0].exercise + props.parts[1].exercise + props.parts[2].exercise} />
   )
 }
 
@@ -35,26 +35,11 @@ const App = () => {
     {part: 'Using props to pass data', exercise: 7},
     {part: 'State of a component', exercise: 14}
   ]
-    
-
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
+  
   return (
     <div>
       <Header course={course}/>
-
-      <Content part={part1} exercise={exercises1}/>
-
-      <Content part={part2} exercise={exercises2}/>
-
-      <Content part={part3} exercise={exercises3}/>
-
-      <Total noExercises={exercises1 + exercises2 + exercises3}/>
+      <Content parts={parts}/>
     </div>
   )
 }
