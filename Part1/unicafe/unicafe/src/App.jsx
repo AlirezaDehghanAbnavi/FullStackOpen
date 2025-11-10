fdb52793f4a12c6abc1e006662f9fd3e111e21a8
Alireza Dehghanabnavi <alirezadehghan93@yahoo.com>
2025-11-10 20:29:19 +0100

    Basic

---
+++ Part1/unicafe/unicafe/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+const Button = (props) => (
+  <button onClick={props.onClick}>{props.text}</button>
+)
+
+const App = () =>
+
+  <div>
+    <h1>give feedback</h1>
+  </div>
+
+
+export default App
