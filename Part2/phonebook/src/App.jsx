f69f6e944a49c7d25f95aa23e30dd2d536d2afd6
Alireza Dehghanabnavi <s308048@studenti.polito.it>
2025-12-13 19:16:16 +0100

    2.16 Done

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -4,48 +4,57 @@ import Person from './components/Person'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import CommunicationService from './services/CommunicationService'
+import Notification from './components/Notification'
 
 const App = () => {
-  const [persons, setPersons] = useState([]) 
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('');
   const [searchedPerson, setSearchedPerson] = useState('');
-  
+  const [notification, setNotification] = useState(null);
+
   const addName = (event) => {
     event.preventDefault()
 
     if (!newName) {
       alert("Name cannot be empty");
-    return;
+      return;
     }
 
     if (!newNumber) {
       alert("Number cannot be empty");
-    return;
+      return;
     }
 
     const personObject = {
       name: newName,
       number: newNumber,
-      id: String(persons.length+1)
+      id: String(persons.length + 1)
     }
 
     const existingPerson = persons.find(p => p.name === newName);
 
-    if (existingPerson){
-      if (existingPerson.number !== newNumber){
+    if (existingPerson) {
+      if (existingPerson.number !== newNumber) {
         numberHandler(existingPerson.id, existingPerson)
       } else {
         alert(`${newName} is already added to phonebook`)
-        }
+      }
       return
     }
-    
+
     CommunicationService.create(personObject).then((returnedPerson) => {
       setPersons(persons.concat(returnedPerson))
       setNewName('')
       setNewNumber('')
     })
+
+    setNotification(
+      `Added ${personObject.name}`
+    )
+    setTimeout(() => {
+      setNotification(null)
+    }, 5000)
 
 
   }
@@ -66,38 +75,41 @@ const App = () => {
 
   useEffect(() => {
     CommunicationService.getAll()
-                        .then(initialPhoneBook => {
-                          console.log("Promise fullfilled")
-                          setPersons(initialPhoneBook)
-                        })
+      .then(initialPhoneBook => {
+        console.log("Promise fullfilled")
+        setPersons(initialPhoneBook)
+      })
   }, [])
 
   const deleteHandler = (id, person) => {
-    if(confirm(`Delete ${person}?`)){
+    if (confirm(`Delete ${person}?`)) {
       CommunicationService.deleteFromDB(id)
-                          .then(() => {
-                          console.log("Delete request fullfilled")
-                          setPersons(persons.filter(p=>p.id !== id))
-                        }).catch(error => alert(error))}
+        .then(() => {
+          console.log("Delete request fullfilled")
+          setPersons(persons.filter(p => p.id !== id))
+        }).catch(error => alert(error))
+    }
   }
 
   const numberHandler = (id, person) => {
-    if(confirm(`${person.name} is already addded to phonebook, replace the old number with a new one?`)){
+    if (confirm(`${person.name} is already addded to phonebook, replace the old number with a new one?`)) {
       const updatedPerson = { ...person, number: newNumber };
       CommunicationService.updateNumber(id, updatedPerson)
-                          .then((returnedPerson) => {
-                          setPersons(persons.map(p => p.id === id ? returnedPerson : p))
-                          setNewName('')
-                          setNewNumber('')
-                          }).catch(error => alert(error))}
+        .then((returnedPerson) => {
+          setPersons(persons.map(p => p.id === id ? returnedPerson : p))
+          setNewName('')
+          setNewNumber('')
+        }).catch(error => alert(error))
+    }
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-        <Filter searchedPerson={searchedPerson} handleSearchChange={handleSearchChange}/>
+      <Notification message={notification}/>
+      <Filter searchedPerson={searchedPerson} handleSearchChange={handleSearchChange} />
       <h2>add a new</h2>
-      <PersonForm 
+      <PersonForm
         addName={addName}
         newName={newName}
         handleNameChange={handleNameChange}
@@ -105,7 +117,7 @@ const App = () => {
         handleNumberChange={handleNumberChange}
       />
       <h2>Numbers</h2>
-      <Person persons={personToShow} deleteHandler={deleteHandler} />      
+      <Person persons={personToShow} deleteHandler={deleteHandler} />
     </div>
   )
 }
--- Part2/phonebook/src/services/CommunicationService.jsx
+++ Part2/phonebook/src/services/CommunicationService.jsx
@@ -1,21 +1,21 @@
-import axios from "axios";  
+import axios from "axios";
 const baseUrl = "http://localhost:3001/persons"
 
 const getAll = () => {
     return axios.get(baseUrl).then(response => response.data);
 }
 
-const create = ( person ) => {
+const create = (person) => {
     const request = axios.post(baseUrl, person)
     return request.then(response => response.data)
 }
 
-const deleteFromDB = ( id ) => {
+const deleteFromDB = (id) => {
     const request = axios.delete(`${baseUrl}/${id}`)
     return request.then(response => response.data)
 }
 
-const updateNumber = ( id, person ) => {
+const updateNumber = (id, person) => {
     const request = axios.put(`${baseUrl}/${id}`, person)
     return request.then(response => response.data)
 }
