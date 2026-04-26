ab8443f6b940eca6ab2398c2020cb7fca38c58e8
Alireza Dehghanabnavi <alirezadehghan93@yahoo.com>
2026-04-26 15:25:40 +0200

    3.2 Done

--- Part3/Phonebook_backend/index.js
+++ Part3/Phonebook_backend/index.js
@@ -30,6 +30,16 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+  const timestamp = new Date();
+  const noPeople = persons.length || 0;
+
+  response.send(`
+    <p>Phonebook has info for ${noPeople} people</p>
+    <p>${timestamp}</p>
+    `);
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
