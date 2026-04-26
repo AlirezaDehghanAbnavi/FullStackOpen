61cf07a0f8c6832ea2a4d48ac14961029081ff18
Alireza Dehghanabnavi <alirezadehghan93@yahoo.com>
2026-04-26 15:43:24 +0200

    3.3 Done

--- Part3/Phonebook_backend/index.js
+++ Part3/Phonebook_backend/index.js
@@ -40,6 +40,17 @@ app.get('/info', (request, response) => {
     `);
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id;
+  const personToShow = persons.find(person => person.id === id);
+  if (personToShow) {
+    response.json(personToShow)
+  } else {
+    console.log(`Person with ID:{${id}} does not exist`);
+    response.status(404).end();
+  }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
